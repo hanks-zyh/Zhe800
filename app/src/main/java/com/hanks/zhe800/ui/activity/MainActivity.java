e698e118439dba1c7e297851b81dc5ce9ee8f5a8
hanks-zyh <zhangyuhan2014@gmail.com>
2015-09-29 18:32:57 +0800

    finish bottom tab in activity_main.xml

--- app/src/main/java/com/hanks/zhe800/ui/activity/MainActivity.java
+++ app/src/main/java/com/hanks/zhe800/ui/activity/MainActivity.java
@@ -1,7 +1,16 @@
 package com.hanks.zhe800.ui.activity;
 
+import android.os.Bundle;
+
+import com.hanks.zhe800.R;
+
 /**
  * Created by Hanks on 2015/9/28.
  */
 public class MainActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
