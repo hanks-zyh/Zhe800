b609a86af4348cb278dc5d6db872859d990a0461
hanks-zyh <zhangyuhan2014@gmail.com>
2015-09-30 12:15:46 +0800

    add support-v13

--- app/src/main/java/com/hanks/zhe800/ui/activity/MainActivity.java
+++ app/src/main/java/com/hanks/zhe800/ui/activity/MainActivity.java
@@ -1,16 +1,49 @@
 package com.hanks.zhe800.ui.activity;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 
 import com.hanks.zhe800.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Hanks on 2015/9/28.
  */
-public class MainActivity extends BaseActivity{
+public class MainActivity extends BaseActivity {
+
+    @Bind(R.id.vp_main)
+    ViewPager vp_main;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initViewPager();
     }
+
+    private void initViewPager() {
+//        vp_main.setAdapter();
+    }
+
+    public void onTagClick(View view) {
+        switch (view.getId()) {
+            case R.id.rb_jinri:
+                break;
+            case R.id.rb_category:
+                break;
+            case R.id.rb_brand:
+                break;
+            case R.id.rb_jifen:
+                break;
+            case R.id.rb_user:
+                break;
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/hanks/zhe800/ui/adapter/MainPageAdapter.java
@@ -0,0 +1,7 @@
+package com.hanks.zhe800.ui.adapter;
+
+/**
+ * Created by Hanks on 2015/9/30.
+ */
+public class MainPageAdapter  {
+}
